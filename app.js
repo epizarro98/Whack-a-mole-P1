ec34883e93d92004f2809bcb3f090fcec3876b83
epizarro98 <epizarro309@gmail.com>
2020-09-24 17:16:58 -0400

    First steps

---
+++ app.js
@@ -0,0 +1,15 @@
+//Make 3x3 grid, then turn them into circles
+//make 9 divs inside of a container and give them a class to access later
+//Add a click event listener to each div
+//make mole
+
+
+
+//handling the circles click event
+const pocketClick = () => {
+    console.log('working')
+}
+
+
+
+let pocket = document.querySelector('.moles--home').addEventListener('click', pocketClick)
