5f918cd4404b5c63481abe2420199fcf40d22b59
epizarro98 <epizarro309@gmail.com>
2020-09-28 20:56:52 -0400

    added mole image

--- app.js
+++ app.js
@@ -25,6 +25,19 @@
     
     document.addEventListener('DOMContentLoaded', () => {
         const start = document.querySelector('.start')
+        const timer = document.querySelector('.timer')
+        timerLeft = 30
+        //countdown function
+        const countdown = () => {
+            setInterval(() => {
+            if(timerLeft <= 0){
+                clearInterval(timerLeft = 0)
+            }
+                timer.innerHTML = 'Timer: ' + timerLeft
+                timerLeft -=1
+            }, 1000);
+        }
+
         
         const timeout = () => {
             turnRed(getRandomDiv())
@@ -34,31 +47,38 @@
         const getRandomDiv = () => {
             let randNum = (Math.floor(Math.random() * 9)).toString() 
             return document.getElementById(randNum)
+            //to get image set background image? try it
         }
+
         //not every 3 secs but after 3 secs; 1 at a time
         //div parameter is.....(answer here: )
         const turnRed = (div) => {
-            div.style.backgroundColor = 'red'
+            div.style.backgroundImage = "url('./Assets/mole.jpg')";
+            div.style.backgroundPosition = 'center'
+            div.style.backgroundSize = 'cover'
             setTimeout(turnBurlywood, 500, div)
         }
+        //conditional to see if image is there, then count++
         //randnum=1-8 divs, keeping track using randNum
-        
+        //
         const turnBurlywood = (div) => {
+            div.style.backgroundImage = ''
             div.style.backgroundColor ='burlywood'
             timeout()
         }
         
         // timeout()
         start.addEventListener('click', timeout)
+        start.addEventListener('click', countdown)
         
     })
-        // setTimeout((getBack2(colorChange)), 1000)
-        // setTimeout(getBack, 3000, randNum)
+    // setTimeout((getBack2(colorChange)), 1000)
+    // setTimeout(getBack, 3000, randNum)
     // debugger
     // count++
-
-
-
+    
+    
+    
 
 
 
